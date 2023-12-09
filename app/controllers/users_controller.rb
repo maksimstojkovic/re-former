c1deb068f92db9b06a4ae7965f5bba18be5da2fe
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-09 12:47:28 +1100

    Add edit and update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to edit_user_path(@user)
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to edit_user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
